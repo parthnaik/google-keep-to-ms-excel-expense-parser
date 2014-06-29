b51073d9614c4bec450d566eeadb0b54cfd6e6dc
Parth <parth_naik91@hotmail.com>
2014-06-29 17:27:08 +0530

    Repaired

--- keep_to_excel_parser.rb
+++ keep_to_excel_parser.rb
@@ -4,7 +4,6 @@ def open_file(filepath)
 end
 
 def check_if_expense(string)
-
 	string.include?(" - ") ? true : false
 end
 
@@ -31,18 +30,18 @@ parsed_expenses = []
 expenses.each_with_index  do |line, index|
 
 	if check_if_expense(line) && check_if_first_expense(expenses, index)
-		expenses_hash = {} # create new hash
+		expenses_array = [] # create new array for hashes
 		i = index # set i as index counter
 
 		# sets key value pairs in hash until the end of that particular expenses block
 		until check_if_expense(expenses[i]) == false
 			type, cost = split_string_on_char(expenses[i], " - ")
-			expenses_hash[type] = cost.chomp
+			expenses_array << { type => cost.chomp }
 			i += 1
 		end
 
-		# pushes the newly created hash to the new array
-		parsed_expenses << expenses_hash
+		# pushes the newly created array of hashes to the new array
+		parsed_expenses << expenses_array
 	elsif check_if_expense(line) == false
 		parsed_expenses << line
 	end
@@ -53,15 +52,11 @@ excel_parsed = create_text_file("#{filename}_parsed")
 
 parsed_expenses.each do |element|
 
-	if element.class == Hash
-		element.each_key { |key| File.open(excel_parsed, "a") { |f| f << key + "\n" } }
-		element.each_value { |value| File.open(excel_parsed, "a") { |f| f << value + "\n" } }
+	if element.class == Array
+		element.each { |hash| File.open(excel_parsed, "a") { |f| f << hash.keys[0] + "\n" } }
+		element.each { |hash| File.open(excel_parsed, "a") { |f| f << hash.values[0] + "\n" } }
 	else
 		File.open(excel_parsed, "a") { |f| f << element } if element.class == String
 	end
 
 end
-
-
-
-
